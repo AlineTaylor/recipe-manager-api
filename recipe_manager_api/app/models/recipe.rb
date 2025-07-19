ef3c1c2a0760fa4ee42cc4f100746e13752b9379
Aline Taylor <alineyui.taylor@gmail.com>
2025-07-19 17:16:58 -0500

    Refactor validations and associations in Recipe and User models for improved clarity

--- recipe_manager_api/app/models/recipe.rb
+++ recipe_manager_api/app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+# validations
   validates :title, presence: true,length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
   validates :servings, :cooking_time, presence: true, numericality: { only_integer: true }, length: { maximum: 3 }
   attribute :favorite, :shopping_list, default: false
+  # associations
+  belongs_to :user
 end
--- recipe_manager_api/app/models/user.rb
+++ recipe_manager_api/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :recipes
+# validations
   validates :email, presence: true, uniqueness: true, confirmation: true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
   validates :email_confirmation, presence: true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
   validates :password, presence: true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   def measurement_systems
     %w(metric imperial)
   end
+
+  # associations
+  has_many :recipes, dependent:destroy
 end
