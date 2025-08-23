214b5c8d7ec9a3e962761afacf9d69aa62d2920b
Aline Taylor <alineyui.taylor@gmail.com>
2025-08-22 23:31:04 -0500

    Added profile picture to user data via active Storage. Updated Blueprint and controller accordingly.

--- recipe_manager_api/app/blueprints/user_blueprint.rb
+++ recipe_manager_api/app/blueprints/user_blueprint.rb
@@ -6,7 +6,15 @@ class UserBlueprint < Blueprinter::Base
 
   # views
   view :normal do
-    fields :id, :email, :first_name, :last_name, :preferred_system
+      fields :id, :email, :first_name, :last_name, :preferred_system
+      field :profile_picture_url do |user, _options|
+        # if the user has a profile picture attached, the payload will include the URL
+        if user.profile_picture.attached?
+          Rails.application.routes.url_helpers.rails_blob_url(user.profile_picture, only_path: true)
+        else
+          nil
+        end
+      end
   end
 
 end
